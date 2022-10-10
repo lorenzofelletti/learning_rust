71f551e6ab22ed06d8508b50b484d9019fa91d1a
lorenzofelletti <lorenzo97.felletti@gmail.com>
2022-10-10 15:31:41 +0200

    add example using channels

---
+++ channels_example/src/main.rs
@@ -0,0 +1,31 @@
+use std::{sync::mpsc, thread};
+
+static NTHREADS: i32 = 3;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let mut children = Vec::new();
+
+    for id in 0..NTHREADS {
+        let thread_tx = tx.clone();
+
+        let child = thread::spawn(move || {
+            thread_tx.send(id).unwrap();
+
+            println!("thread {id} finished");
+        });
+
+        children.push(child);
+    }
+
+    let mut ids = Vec::with_capacity(NTHREADS as usize);
+    for _ in 0..NTHREADS {
+        ids.push(rx.recv());
+    }
+
+    for child in children {
+        child.join().expect("oops! the child thread panicked");
+    }
+
+    println!("{:?}", ids);
+}
